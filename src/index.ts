93b4f864744896298d89377f07cdde067e0332e5
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-02 15:04:31 -0300

    create errors handler from  yup and general errors

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import dotenv from 'dotenv'
 dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'})
 import './database/connection';
 import "reflect-metadata";
+import 'express-async-errors'
 
 import cors from 'cors';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333);
